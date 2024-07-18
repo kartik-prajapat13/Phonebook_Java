07bfedfbcd6ffb33d8e30233891fe76340187a6d
Kartik Prajapat <105015520+kartik-prajapat13@users.noreply.github.com>
2024-07-18 15:03:33 +0530

    Update User.java

--- Phonebook/Phonebook_Project-master/src/main/java/com/dev/phonebook/entity/User.java
+++ Phonebook/Phonebook_Project-master/src/main/java/com/dev/phonebook/entity/User.java
@@ -1,9 +1,3 @@
-/*   Created by IntelliJ IDEA.
- *   Author: Devansh Shukla (D-Coder135)
- *   Date: 12-07-2022/07/2022
- *   Time: 01:45 PM
- *   File: User
- */
 
 package com.dev.phonebook.entity;
 
