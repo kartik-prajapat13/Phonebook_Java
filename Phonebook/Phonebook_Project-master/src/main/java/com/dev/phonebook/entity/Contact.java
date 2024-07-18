13dfa9bd3a6073fe13a8b9b0909c99c1bb946726
Kartik Prajapat <105015520+kartik-prajapat13@users.noreply.github.com>
2024-07-18 15:03:15 +0530

    Update Contact.java

--- Phonebook/Phonebook_Project-master/src/main/java/com/dev/phonebook/entity/Contact.java
+++ Phonebook/Phonebook_Project-master/src/main/java/com/dev/phonebook/entity/Contact.java
@@ -1,9 +1,4 @@
-/*   Created by IntelliJ IDEA.
- *   Author: Devansh Shukla (D-Coder135)
- *   Date: 12-07-2022/07/2022
- *   Time: 01:49 PM
- *   File: Contact
- */
+
 
 package com.dev.phonebook.entity;
 
